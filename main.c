78e2c35441b89f9abafb3bf25ca1520697656d43
jeeban anand <jeeban29@gmail.com>
2015-02-23 17:22:03 +0530

    Update main.c
    remove unwanted lines

--- main.c
+++ main.c
@@ -5,21 +5,9 @@
 #
 ####################################################################################################*/
 
-/*///////////////////////////////////////////////////////////////////////////////////////////////////
-changes to be done.
-
--> add_point() syntax has been changed.
-	char* add_point( char*, int ) --> void add_point( char*, int )
-
--> review all the function declaration
-
-
-
-*//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include"headers.c"
-//#include"sample.c"
-//#include"test.c"
+
 
 int main(int main_argc,char **main_argv )
 {
@@ -59,12 +47,6 @@ int main(int main_argc,char **main_argv )
 	
 	printf("final calculation := %s\n\n", temp_memory[19]);
 	printf( "length = %d\n", string_length( temp_memory[19] ) );
-
-
-
-
-
-
 
 
 
@@ -135,117 +117,3 @@ int main(int main_argc,char **main_argv )
 
 
 
-
-
-
-
-
-
-
-
-
-/*	int i,counter0=0,counter1=0,counter2=0,counter3=0,counter4=0;
-	
-	//step 1 of the process :- error checking in the expression.
-	check(argc,argv);
-	//error checking is completed. 
-	
-	//report the input numbers sizes.
-	for( i=1; i<argc; i+=2 )
-	{
-		if( *(argv[i]) == '-')
-		{
-			printf("argv[%d] = %d\t-ve\n",i,string_length(argv[i]) - 1);
-		}
-		else
-		{
-			printf("argv[%d] = %d\t+ve\n",i,string_length(argv[i]));
-		}		
-	}
-	//sizes have been reported.
-	
-	//report the total no of signs used in the syntax.
-	for( i=2; i<argc; i+=2 )
-	{
-		if( less( argv[i],"+" ) == 0 )
-		{
-			counter0++;
-		}
-		else if( less( argv[i],"-" ) == 0 )
-		{
-			counter1++;
-		}
-		else if( less( argv[i],"x" ) == 0 )
-		{
-			counter2++;
-		}
-		else if( less( argv[i],"/" ) == 0 )
-		{
-			counter3++;
-		}
-		else if( less( argv[i],"%" ) == 0 )
-		{
-			counter4++;
-		}			
-	}
-	
-	printf("\n\naddition 	:= %d\nsubtraction 	:= %d\nmultiplication 	:= %d\ndivision	:= %d\nmod division 	:= %d\nfactorial	:= %d\n\n\n",counter0,counter1,counter2,counter3,counter4,fact_count);		
-	//sign count completed.
-		
-	//step 2 of process :- analyze any factorial part in the expression.	
-	process_fact(argc,argv);
-	//step 2 completed.
-	
-	//reinitialize the final_mul and sum as it is changed by the factorial calculation phase.
-	final_mul = "1";
-	final = "0";
-	
-	//step 3 of process :- perform multiplication and division works.
-	if(argc > 2)
-	{
-		process_mul(argc,fact_array);
-		free( fact_array );
-	}
-	else
-	{
-		final_mul_array = fact_array;
-		fact_array = NULL;
-	}
-	//multiplication complited.
-	
-	//step 4 of process :- sign analysis part
-	process_sign();
-	//done now the final addition and subtraction are to be done.
-		
-	if( argc == 2)
-	{
-		final = final_mul_array[1];
-	}
-	else
-	{	
-		//evaluate the expression
-		
-		final = final_mul_array[1];
-		for( i=3; i<final_mul_array_pointer; i+=2 )
-		{
-			if( compare( final_mul_array[i-1], "+" ) == 0 )
-			{
-				add( final_mul_array[i] );
-			}
-			else
-			{
-				subtract( final_mul_array[i] );
-			}
-		}
-	
-	}
-	
-	//expression evaluated successfully . time to display the result.
-	printf("\n\nfinal calculation 	:= %s\n\n",final);
-	
-	printf("length = %d\n\n",string_length(final));
-	
-	free(final_mul_array);	
-*/
-//	return 0;
-//}
